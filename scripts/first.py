4eb3b62ecf6bb6f46dcb3d20b0332e11ea9d4b9b
Renato Fabbri <renato.fabbri@adroll.com>
2020-01-02 16:57:30 -0300

    first example data, script and resulting image

---
+++ scripts/first.py
@@ -0,0 +1,29 @@
+import networkx as x
+
+with open('../data/first.txt', 'r') as f:
+    ppl = f.read().split('\n')[:-1]
+
+nodes = {
+    'teachers': [],
+    'who': '',
+    'students': []
+}
+
+for p in ppl:
+    p_ = p.split(' ')
+    quality = int(p_[0])
+    name = ' '.join(p_[1:])
+    if quality > 0:
+        nodes['teachers'].append(name)
+    elif quality == 0:
+        nodes['who'] = name
+    else:
+        nodes['students'].append(name)
+
+
+g = x.DiGraph()
+
+for t in nodes['teachers']:
+    g.add_edge(t, nodes['who'])
+for s in nodes['students']:
+    g.add_edge(nodes['who'], s)
